fe7e9da51ca084e3d23fcc55be6ae6a5b887c932
Paul's Repository <cnubigdata@gmail.com>
2025-05-24 04:36:33 +0900

    Update pipeline_dataset_test.py

--- pipeline_dataset_test.py
+++ pipeline_dataset_test.py
@@ -41,3 +41,15 @@ print("> Model Output:", outputs)
 
 probabilities = F.softmax(outputs.logits, dim=1)
 print(probabilities)
+
+# Zero-shot classification
+# Test 4 ##########################################################################
+from transformers import pipeline
+
+classifier = pipeline("zero-shot-classification")
+classifier(
+    "This is a course about the Transformers library",
+    candidate_labels=["education", "politics", "business"],
+)
+
+
