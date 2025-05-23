6d6ea86ba87c25e65c8b29bc824b6f31057b3a65
Paul's Repository <cnubigdata@gmail.com>
2025-05-24 04:15:55 +0900

    Update pipeline_test.py

--- pipeline_test.py
+++ pipeline_test.py
@@ -5,3 +5,13 @@ classifier = pipeline("sentiment-analysis")
 result = classifier("I love Hugging Face!")
 
 print(result)
+
+sentiment = pipeline("sentiment-analysis")
+print(sentiment("Hugging Face is amazing!"))
+
+from datasets import load_dataset
+dataset = load_dataset("imdb")
+print(dataset["train"][0])
+
+
+
